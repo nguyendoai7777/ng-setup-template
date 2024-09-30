40e122aad5aaa3cf60e733ae21c9dee63c4b9c34
doai <doai.nguyen@sotatek.com>
2024-09-30 11:57:13 +0700

    upgrage to ver 18.2.6

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { interceptor } from '@interceptor/app.interceptor';
 import { catchingErrorInterceptor } from '@interceptor/catching-error.interceptor';
@@ -13,7 +12,8 @@ export const appConfig: ApplicationConfig = {
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideHttpClient(withInterceptors([interceptor(), catchingErrorInterceptor()])),
 		provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
-		provideClientHydration(),
+		provideAnimationsAsync(),
+		provideAnimationsAsync(),
 		provideAnimationsAsync()
 	]
 };
