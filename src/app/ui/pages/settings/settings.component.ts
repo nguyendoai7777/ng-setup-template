c28e9926cb26a3c57d312d3224f79566bc7b018c
doai <doai.nguyen@sotatek.com>
2025-01-20 10:35:30 +0700

    update to v19

--- src/app/ui/pages/settings/settings.component.ts
+++ src/app/ui/pages/settings/settings.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-	selector: 'settings',
-	standalone: true,
-	imports: [],
-	templateUrl: './settings.component.html',
-	styleUrl: './settings.component.scss',
-	changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'settings',
+  imports: [],
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SettingsComponent {}
